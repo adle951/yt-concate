7da49bc9cb38c942b324931c5ce3cf26eaede396
jing chen <adle9511@gmail.com>
2022-09-12 14:25:14 +0800

    Refactor code into pipeline design pattern

---
+++ yt_concate/pipeline/pipeline.py
@@ -0,0 +1,14 @@
+from yt_concate.pipeline.steps.step import StepException
+
+class Pipeline:
+    def __init__(self, steps):
+        self.steps = steps
+
+    def run(self, inputs):
+        for step in self.steps:
+            data = None
+            try:
+                data = step.process(data, inputs)
+            except StepException as e:
+                print('Exception happened at', e)
+                break
