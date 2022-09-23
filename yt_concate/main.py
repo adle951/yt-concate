5f73998197fdba73ff83ffaba01c7c6b253c6401
jing chen <adle9511@gmail.com>
2022-09-23 10:42:44 +0800

    successfully read captions and into data and fix step

--- yt_concate/main.py
+++ yt_concate/main.py
@@ -1,12 +1,12 @@
 from yt_concate.pipeline.steps.get_video_list import GetVideoList
 from yt_concate.pipeline.pipeline import Pipeline
 from yt_concate.pipeline.steps.download_captions import DownloadCaptions
+from yt_concate.pipeline.steps.read_caption import ReadCaption
 from yt_concate.utils import Utils
 from yt_concate.pipeline.steps.preflight import Preflight
 from yt_concate.pipeline.steps.postflight import Postflight
 
 CHANNEL_ID = 'UCKSVUHI9rbbkXhvAXK-2uxA'
-
 
 
 def main():
@@ -17,6 +17,7 @@ def main():
         Preflight(),
         GetVideoList(),
         DownloadCaptions(),
+        ReadCaption(),
         Postflight()
 
     ]
